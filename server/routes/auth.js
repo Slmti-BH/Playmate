9e5ce6ecbab3309f5d90d0035533bbd1b3d42271
Selamawit Hailemariam <selamatwits@gmail.com>
2022-01-20 07:52:42 -0800

    Add server-side authentication and  authorization, also add json file for users details.

---
+++ server/routes/auth.js
@@ -0,0 +1,113 @@
+const { Router } = require("express");
+const authRoutes = Router();
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+//to read from users.json
+const readData = () => {
+  const videosData = fs.readFileSync("./data/auth.json");
+  return JSON.parse(videosData);
+};
+
+// to write to videos.json
+const writeFile = (videosData) => {
+  fs.writeFileSync("./data/auth.json", JSON.stringify(videosData));
+};
+
+// check we have all required fields
+const registerValidation = (req, res, next) => {
+  if (
+    !req.body.username ||
+    !req.body.name ||
+    !req.body.password ||
+    !req.body.email ||
+    !req.body.numberOfChildren
+  ) {
+    res
+      .status(400)
+      .send(
+        "Please make sure to include username, name, password, email and number of children in the form."
+      );
+  } else {
+    next();
+  }
+};
+
+authRoutes.post("/register", registerValidation, (req, res) => {
+  const { username, name, password, email, numberOfChildren } = req.body;
+  const usersData = readData();
+
+  const newUser = {
+    id: uuid(),
+    username: username,
+    name: name,
+    password: password,
+    email: email,
+    numberOfChildren: numberOfChildren,
+  };
+  usersData.push(newUser);
+  //   update usersData so that user information is added
+  writeFile(usersData);
+  res.json({ success: "true" });
+});
+
+// login endpoint
+authRoutes.post("/sign-in", (req, res) => {
+  const { username, password } = req.body;
+
+  const usersData = readData();
+
+  const user = usersData.find((user) => user.username === username);
+
+  // handle the instances where user is not found/is found
+  if (!user) {
+    return res.status(404).send("User not found.");
+  }
+  //check if user entered correct password
+  if (user.password !== password) {
+    return res.status(401).send("Please enter correct password.");
+  } else {
+    const token = jwt.sign({ name: user.name }, JWT_SECRET, {
+      expiresIn: "8h",
+    });
+
+    res.json({ token });
+  }
+});
+
+// authorize middleware
+const authorize = (req, res, next) => {
+  // first check if headers contain authorization
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "not authorized" });
+  }
+  // else check token in headers
+  const authToken = req.headers.authorization.split(" ")[1];
+
+  // decode content of token
+  jwt.verify(authToken, JWT_SECRET, (err, decoded) => {
+    console.log("authorize middleware :: JWT verification");
+    if (err) {
+      return res.status(401).json({ message: "not authorized" });
+    }
+    // check if token is expired
+    if (Date.now() > new Date(decoded.exp * 1000)) {
+      return res.status(401).json({ message: "token expired" });
+    }
+
+    // decoded contents should to be on req.decoded
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// profile endpoint
+authRoutes.get("/profile", authorize, (req, res) => {
+  res.json(req.decoded);
+});
+
+module.exports = authRoutes;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
+const authRoutes=require("./routes/auth")
+// to make .env files available
+require("dotenv").config();
+const PORT = process.env.PORT || 6060;
 
-app.listen(8080, () => {
+// cors middle ware to allow access to requests from client URL
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json());
+
+app.use("/auth",authRoutes);
+
+app.listen(PORT, () => {
   console.log("🚀 Listening on 8080");
 });
